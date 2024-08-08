28c8476616c981a87dbe0aca94ba1ef67274d52f
Joseph Eiba <josepheiba@icloud.com>
2024-08-08 19:46:18 +0100

    commit random

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 -- create table if not exists users
-SELECT origin, fans as nb_fans FROM metal_bands ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
