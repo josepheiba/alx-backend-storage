e04363156cf4c01bcb05fda2bc4d85e742d176e7
Joseph Eiba <josepheiba@icloud.com>
2024-08-15 02:01:24 +0100

    commit random

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,40 @@
+#!/usr/bin/env python3
+"""
+module contains function and decorator function
+that caches requests in redis cache
+"""
 import redis
 import requests
-from typing import Callable
-
-r = redis.Redis()
+from functools import wraps
 
 
-def cache_with_tracking(func: Callable) -> Callable:
+store = redis.Redis()
+
+
+def url_cacher(method):
     """
-    Decorator that caches the result of a function
+    decorator function caches web requests
     """
-    def wrapper(url: str) -> str:
-        count_key = f"count:{url}"
-        r.incr(count_key)
-        cached_content = r.get(url)
-        if cached_content:
-            return cached_content.decode('utf-8')
-        content = func(url)
-        r.setex(url, 10, content)
-        return content
-    return wrapper
+    @wraps(method)
+    def invoker(url):
+        """
+        function returns saves requests and returns cached requests
+        """
+        data = store.get(f"key:{url}")
+        if data:
+            return data.decode("utf-8")
+        count = f"count:{url}"
+        html = method(url)
+        store.incr(count)
+        store.set(url, html)
+        store.expire(url, 10)
+        return html
+    return invoker
 
 
-@cache_with_tracking
+@url_cacher
 def get_page(url: str) -> str:
     """
-    Fetches the HTML content of the given URL.
+    function gets html content
     """
-    response = requests.get(url)
-    return response.text
-
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    print(get_page(url))
-    print(f"Access count: {r.get(f'count:{url}').decode('utf-8')}")
+    return requests.get(url).text
